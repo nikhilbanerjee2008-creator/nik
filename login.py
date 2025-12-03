cb85508e589ee5108364f7f3a3afcf478e1e6afe
nikhilbanerjee2008-creator <nikhilbanerjee2008@gmail.com>
2025-12-03 21:19:23 +0530

    Create login.py

---
+++ login.py
@@ -0,0 +1,9 @@
+#Login function
+def logintype():
+  print("**********WELCOME**********")
+  print("1.Admin login    2.User login")
+  login=int(input("Enter login type:"))
+
+def userlogin():
+  print
+  
